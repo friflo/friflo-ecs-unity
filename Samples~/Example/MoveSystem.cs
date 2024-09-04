eb6922439694647d3db11b35283992737e66647c
Ullrich Praetz <ullrich@praetz.de>
2024-09-04 16:56:45 +0200

    add system example

---
+++ Samples~/Example/MoveSystem.cs
@@ -0,0 +1,20 @@
+﻿using Friflo.Engine.ECS;
+using Friflo.Engine.ECS.Systems;
+
+namespace Friflo.Example
+{
+    public class MoveSystem : QuerySystem<Position>
+    {
+        public System.Numerics.Vector3 move;
+        
+        protected override void OnUpdate()
+        {
+            var delta = Tick.deltaTime;
+            foreach (var (positions, entities) in Query.Chunks) {
+                for (int n = 0; n < entities.Length; n++) {
+                    positions[n].value += delta * move;
+                }
+            }
+        }
+    }
+}
